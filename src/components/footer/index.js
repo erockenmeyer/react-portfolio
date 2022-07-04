bb2ca1f9454d45fbb2f113588c58a2b0ff39f41f
erockenmeyer <erikrockenmeyer@gmail.com>
2022-07-04 00:27:00 -0500

    added basic file structure, started information

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <footer>
+            <h3>erockenmeyer</h3>
+            <span>2022</span>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../navigation";
+
+function Header(props) {
+    return (
+        <header>
+            <h2>Erik Rockenmeyer</h2>
+            <Navigation></Navigation>
+        </header>
+    )
+}
+
+export default Header;
