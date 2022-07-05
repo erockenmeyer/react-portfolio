436713fa87947450f80fc43c20b5b8dc6eec5022
erockenmeyer <erikrockenmeyer@gmail.com>
2022-07-04 21:52:21 -0500

    finished footer and finalized styling

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Footer () {
+function Footer() {
     return (
         <footer className="row justify-content-between text-center">
-            <h3 className="col">erockenmeyer</h3>
-            <h3 className="col">2022</h3>
+            <h3 className="col">
+                <a href="https://www.linkedin.com/in/erik-rockenmeyer-6aa4a311a/">LinkedIn</a>
+            </h3>
+            <h3 className="col"><a href="https://github.com/erockenmeyer">Github</a></h3>
+            <h3 className="col"><a href="https://www.codewars.com/users/erockenmeyer">CodeWars</a></h3>
         </footer>
     )
 }
